eed02b56ac5d8cdbc8db26f255867b243e151584
tubigervie <etubig@uci.edu>
2022-09-24 18:06:07 -0400

    Frontend product service + base rest API service

---
+++ Mango.Web/Services/BaseService.cs
@@ -0,0 +1,83 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Mango.Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public ResponseDto responseModel { get; set; }
+        public IHttpClientFactory httpClientFactory { get; set; } //a factory for creating HttpClient instances to be used in the application
+
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            this.responseModel = new ResponseDto();
+            httpClientFactory = httpClient;
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(true); //Normally, the CLR keeps tabs on objects with a finalizer when they are created (making them more expensive to create). S
+                                       //uppressFinalize tells the GC that the object was cleaned up properly and doesn't need to go onto the finalizer queue.
+                                       //This is specifically to immediately release underlying HttpClient socket as without it, this can lead to socket exhaustion
+        }
+
+        public async Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            try
+            {
+                var client = httpClientFactory.CreateClient("MangoAPI");
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+                client.DefaultRequestHeaders.Clear();
+                
+                if(apiRequest.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),
+                        Encoding.UTF8, "application/json");
+                }
+                HttpResponseMessage apiResponse = null;
+                
+                switch (apiRequest.ApiType)
+                {
+                    case SD.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case SD.ApiType.GET:
+                        message.Method = HttpMethod.Get;
+                        break;
+                    case SD.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case SD.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                }
+
+                apiResponse = await client.SendAsync(message);
+
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                var apiResponseDto = JsonConvert.DeserializeObject<T>(apiContent);
+
+                return apiResponseDto;
+            }
+            catch (Exception e)
+            {
+                var dto = new ResponseDto
+                {
+                    DisplayMessage = "Error",
+                    ErrorMessages = new List<string> { Convert.ToString(e.Message) },
+                    IsSuccess = false
+                };
+
+                var res = JsonConvert.SerializeObject(dto);
+                var apiResponseDto = JsonConvert.DeserializeObject<T>(res);
+
+                return apiResponseDto;
+            }
+        }
+    }
+}
